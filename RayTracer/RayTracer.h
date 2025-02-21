769c611ef307c7092591711f31629522fd2cbfcf
Austin Hillyard <austin.hillyard@gmail.com>
2025-02-20 20:06:03 -0700

    Syncing again

--- RayTracer/RayTracer.h
+++ RayTracer/RayTracer.h
@@ -1,5 +1,16 @@
 class RayTracer {
 
+    private:
+        double pixel_locations;
+
+
+        void buildScreen();
+
+    public:
+        // Takes in a screen size, and the camera position on the z axis. 
+        // Camera is assumed to be on the z axis looking toward the origin, with the screen on the x-y plane
+        RayTracer(unsigned int screen_width, unsigned int screen_height, unsigned int camera_z_position);
+
     
 
 };
